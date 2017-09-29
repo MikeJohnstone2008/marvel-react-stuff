4f44e50574d95a404fca6606a097f25ddc52a3b2
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-09-29 10:22:49 -0700

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Characters from './containers/Characters'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Characters />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+import characters from './Characters'
+
+const marvelApp = combineReducers({
+    characters
+})
+export default marvelApp
